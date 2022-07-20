695aefaf7b2ce30745adeb7120785350fda09d83
onskulit <dislaty@mail.com>
2022-07-20 12:16:38 +0300

    feat: solve 05-objects-tasks besides one

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 
@@ -111,32 +117,55 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
+/*   result: '', */
+
   element(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += value;
+    return this; */
   },
 
   id(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += `#${value}`;
+    return this; */
   },
 
   class(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += `.${value}`;
+    return this; */
   },
 
   attr(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += `[${value}]`;
+    return this; */
   },
 
   pseudoClass(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += `:${value}`;
+    return this; */
   },
 
   pseudoElement(/* value */) {
     throw new Error('Not implemented');
+    /* this.result += `::${value}`;
+    return this; */
   },
 
   combine(/* selector1, combinator, selector2 */) {
     throw new Error('Not implemented');
+    /* this.result += `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this; */
+  },
+
+  stringify() {
+    throw new Error('Not implemented');
+    /* const finalResult = this.result;
+    this.result = '';
+    return finalResult; */
   },
 };
 
